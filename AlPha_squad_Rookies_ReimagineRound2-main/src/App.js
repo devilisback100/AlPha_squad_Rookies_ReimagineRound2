be00532a05245f51912a14a3e8ffdfd5de81e24d
sumitjha33 <sumitjha3333@gmail.com>
2024-06-26 21:26:36 +0530

    Add files via upload

---
+++ AlPha_squad_Rookies_ReimagineRound2-main/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { Navbar } from './Component_list';
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ AlPha_squad_Rookies_ReimagineRound2-main/src/Component_list.js
@@ -0,0 +1,2 @@
+export {default as  Navbar } from "./Components/navbar/Navbar"
+export {default as amul_logo} from  "./Assets/amul_logo.png"
